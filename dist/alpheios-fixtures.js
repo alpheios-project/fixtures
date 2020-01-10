c20c64e3f1d6584f22de07c55a44fe88bdead8f0
Kirlat <kirlat@yula.media>
2020-01-10 10:40:39 +0400

    Rebuilt after merge

--- dist/alpheios-fixtures.js
+++ dist/alpheios-fixtures.js
@@ -110,22 +110,38 @@ __webpack_require__.r(__webpack_exports__);
 var _cedict_data_zho_cedict_json__WEBPACK_IMPORTED_MODULE_0___namespace = /*#__PURE__*/__webpack_require__.t(/*! @/cedict/data/zho-cedict.json */ "./cedict/data/zho-cedict.json", 1);
 
 
+/**
+ * This class provides stubs that can be used for testing CEDICT functionality of Lexis CS package.
+ * Please see descriptions of each individual method to inspect how each one can be used during testing.
+ */
 class CedictFixture {
   /**
-   * A stub for Cedict._downloadData() that will load local test data instead of a real one.
+   * This stub is supposed to be used for testing a `Cedict` class fo Lexis CS.
+   * Cedict has an internal _downloadData() method that loads CEDICT data from a remote service.
+   * If to replace that method with this stub Cedict class will, instead of going to a remote
+   * service, load a local test sample of CEDICT data bundled into the `fixtures` library.
+   * This will allow to test CEDICT functionality of Lexis CS locally, without sending any network requests.
+   * This stub can also be useful for other tests that involves creation of `Cedict` class instances.
    *
    * @returns {Promise<object>} Returns a promise that is resolved with
-   *          an object containing dictionary data.
+   *          an object containing dictionary data.. Please see Cedict._downloadData() for
+   *          a more detailed information about the format of a return value.
    */
   static downloadData() {
     return Promise.resolve({ meta: _cedict_data_zho_cedict_json__WEBPACK_IMPORTED_MODULE_0__.metadata, dictionary: _cedict_data_zho_cedict_json__WEBPACK_IMPORTED_MODULE_0__.entries })
   }
 
   /**
-   * A stub that imitates a LexisCS CEDICT service response.
-   * @param {string} destName - A request's destination name (not used).
-   * @param {object} request - A LeisCS request.
-   * @returns {Promise<{body: object}>} - A fixture of Lexis CEDICT response.
+   * A stub that imitates a CEDICT `getWords` request sent to a Lexis CS service. It can be used
+   * as a replacement for `sendRequestTo()` method of a Lexis CS `MessagingService`. The stub
+   * employs a test subset of CEDICT data and a body of its return value for the words from this subset
+   * is exactly the same as of a production version of `getWords` CEDICT request.
+   *
+   *
+   * @param {string} destName - A request's destination name. It is not used in a stub and can be of any value.
+   * @param {object} request - A LeisCS request, please consult Lexis CS documentation for a more detailed
+   *        description of its format.
+   * @returns {Promise<{body: object}>} - A response that fully emulates a response of Lexis CS CEDICT service.
    */
   static async lexisCedictRequest(destName, request) {
     // The following stub implements business logic of `getWords()` method from lexis-cs/src/cedict-service/cedict.js
